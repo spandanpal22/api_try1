03adbb13b8ce3482604078269db8b7e86bb761e4
spandan <2spandan2@gmail.com>
2019-05-25 22:02:27 +0530

    first commit

---
+++ api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    body = models.TextField()
+    author = models.ForeignKey('Author', on_delete=None, related_name='articles')
+
+    def __str__(self):
+        return self.title
